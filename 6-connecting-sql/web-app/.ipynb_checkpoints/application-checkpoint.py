0f3eaf27ec6963502f4b90559aae98f39504b800
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-02-24 20:44:25 -0500

    update flask code

--- 6-connecting-sql/web-app/.ipynb_checkpoints/application-checkpoint.py
+++ 6-connecting-sql/web-app/.ipynb_checkpoints/application-checkpoint.py
@@ -2,16 +2,35 @@
 import sqlite3
 from flask import Flask, render_template
 from player import Player
+import os
 
 app = Flask(__name__, static_folder='public', template_folder='views')
 
+app.config.from_object(__name__) # load config from this file , flaskr.py
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'nhl.db'),
+))
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
 @app.route('/nhl/players/<player_id>')
 def show_player(player_id):
-    conn = sqlite3.connect('nhl.db')
+    conn = get_db()
     cursor = conn.cursor()
     cursor.execute('select * from players where id = ?;', player_id)
     player_details = cursor.fetchone()
     player = Player(*player_details)
-    return str(player.__dict__)
+    return render_template('player-1.html', player = player)
 
 app.run()
--- 6-connecting-sql/web-app/application.py
+++ 6-connecting-sql/web-app/application.py
@@ -1,17 +1,32 @@
 # application.py
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from player import Player
+import os
+import pdb
 
 app = Flask(__name__, static_folder='public', template_folder='views')
 
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'nhl.db'),
+))
+
 @app.route('/nhl/players/<player_id>')
 def show_player(player_id):
-    conn = sqlite3.connect('nhl.db')
+    conn = sqlite3.connect(app.config['DATABASE'])
     cursor = conn.cursor()
     cursor.execute('select * from players where id = ?;', player_id)
     player_details = cursor.fetchone()
     player = Player(*player_details)
     return render_template('player-1.html', player = player)
 
-app.run()
+@app.route('/nhl/players')
+def players():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    cursor = conn.cursor()
+    cursor.execute('select * from players;')
+    player_rows = cursor.fetchall()
+    players = [Player(*player_row) for player_row in player_rows]
+    return render_template('players.html', players = players)
+app.run(debug = True)
